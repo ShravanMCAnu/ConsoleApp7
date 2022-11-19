b27324ea571c458af603ec3b8349cb367c4565be
ShravanMCAnu <bskmc850@gmail.com>
2022-11-19 16:08:37 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StudentClassLibrary;
+
+Console.WriteLine("Hello, World!");
+StudentGrade obj = new StudentGrade();
+obj.StudentDetails();
+obj.MarksCalculation();
+obj.CalculateAverage();
+obj.DisplayAll();
